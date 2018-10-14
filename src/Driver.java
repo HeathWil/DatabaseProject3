85df6745f0d50f17165f96b2b968bfc899fee282
HeathWil <Heather.o.wilson@ou.edu>
2018-10-13 19:45:35 -0500

    added jdbc string

--- src/Driver.java
+++ src/Driver.java
@@ -9,7 +9,8 @@ public class Driver {
 	public static void main(String args[]){
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc", "root","root");
+			Connection con = DriverManager.getConnection("jdbc:sqlserver://113300974.database.windows.net:1433;database=Project2;user=Eric71@113300974;password=Soundwave24;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;",
+					"root","root");
 			Statement stmt = con.createStatement();
 			ResultSet rs= stmt.executeQuery("select * from a;jsdflkj");
 			
