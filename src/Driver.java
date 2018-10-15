5c3a73dcefaf00a7a259d9db03d97e0aa6e15f9c
Eric Morales <Eric Morales>
2018-10-14 20:53:56 -0500

    Updated to com.mysql.cj.jdbc.Driver

--- src/Driver.java
+++ src/Driver.java
@@ -11,7 +11,7 @@ public class Driver {
 	
 	public static void main(String args[]){
 		try{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:sqlserver://113300974.database.windows.net:1433;database=Project2;user=Eric71@113300974;password=Soundwave24;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;",
 					"Eric71","Soundwave24");
 			Statement stmt = con.createStatement();
